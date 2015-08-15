53857319c85afaa468f0d7189ff9817d610e75fa
Scott Addie <tobias.addie@gmail.com>
2015-08-15 16:46:52 -0500

    removed unnecessary using blocks and enforced quote consistency in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 /// <binding Clean='clean' />
 'use strict';
-var args = require("yargs").argv,
-  gulp = require("gulp"),
-  rimraf = require("rimraf"),
-  $ = require("gulp-load-plugins")({
+var args = require('yargs').argv,
+  gulp = require('gulp'),
+  rimraf = require('rimraf'),
+  $ = require('gulp-load-plugins')({
     lazy: true
   }),
-  project = require("./project.json");
+  project = require('./project.json');
 
 var paths = {
-  webroot: "./" + project.webroot + "/"
+  webroot: './' + project.webroot + '/'
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.js = paths.webroot + 'js/**/*.js';
+paths.minJs = paths.webroot + 'js/**/*.min.js';
+paths.css = paths.webroot + 'css/**/*.css';
+paths.minCss = paths.webroot + 'css/**/*.min.css';
+paths.concatJsDest = paths.webroot + 'js/site.min.js';
+paths.concatCssDest = paths.webroot + 'css/site.min.css';
 
-gulp.task("clean:js", function(callback) {
+gulp.task('clean:js', function(callback) {
   clean(paths.concatJsDest, callback);
 });
 
-gulp.task("clean:css", function(callback) {
+gulp.task('clean:css', function(callback) {
   clean(paths.concatCssDest, callback);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task('clean', ['clean:js', 'clean:css']);
 
 /**
  * Optimizes JavaScript for the production environment.
@@ -36,17 +36,17 @@ gulp.task("clean", ["clean:js", "clean:css"]);
  *
  * --verbose: Displays the files matched by the globbing pattern.
  */
-gulp.task("min:js", function() {
-  gulp.src([paths.js, "!" + paths.minJs], {
-      base: "."
+gulp.task('min:js', function() {
+  gulp.src([paths.js, '!' + paths.minJs], {
+      base: '.'
     })
     .pipe($.if(args.verbose, $.size({
       showFiles: true,
-      title: "JavaScript"
+      title: 'JavaScript'
     })))
     .pipe($.concat(paths.concatJsDest))
     .pipe($.uglify())
-    .pipe(gulp.dest("."));
+    .pipe(gulp.dest('.'));
 });
 
 /**
@@ -56,18 +56,18 @@ gulp.task("min:js", function() {
  *
  * --verbose: Displays the files matched by the globbing pattern.
  */
-gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
+gulp.task('min:css', function() {
+  gulp.src([paths.css, '!' + paths.minCss])
     .pipe($.if(args.verbose, $.size({
       showFiles: true,
-      title: "CSS"
+      title: 'CSS'
     })))
     .pipe($.concat(paths.concatCssDest))
     .pipe($.cssmin())
-    .pipe(gulp.dest("."));
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('min', ['min:js', 'min:css']);
 
 /**
  * Cleans the specified file path.
