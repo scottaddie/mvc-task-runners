5630b6655779e8f9b00cde25beefe85da200d55d
Scott Addie <tobias.addie@gmail.com>
2015-10-22 10:43:38 -0500

    Refactored gulpfile.js and added documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /// <binding Clean='clean' />
 'use strict';
+
 var args = require('yargs').argv,
-  gulp = require('gulp'),
-  rimraf = require('rimraf'),
-  $ = require('gulp-load-plugins')({
-    lazy: true
-  }),
-  project = require('./project.json');
+    gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    $ = require('gulp-load-plugins')({
+      lazy: true
+    }),
+    project = require('./project.json');
 
 var paths = {
   webroot: './' + project.webroot + '/'
@@ -18,9 +19,15 @@ paths.css = paths.webroot + 'css/**/*.css';
 paths.minCss = paths.webroot + 'css/**/*.min.css';
 paths.concatJsDest = paths.webroot + 'js/site.min.js';
 paths.concatCssDest = paths.webroot + 'css/site.min.css';
+paths.razorViews = 'Views/**/*.cshtml';
 
+/**
+ * Lints the C# Razor views for misuse of Bootstrap.
+ * @return {Stream}
+ * Example: gulp bootlint
+ */
 gulp.task('bootlint', function(){
-  gulp.src('Views/**/*.cshtml')
+  return gulp.src(paths.razorViews)
     .pipe($.bootlint({
       disabledIds: ['E001', 'E007']
     }));
@@ -44,7 +51,7 @@ gulp.task('clean', ['clean:js', 'clean:css']);
  * --verbose: Displays the files matched by the globbing pattern.
  */
 gulp.task('min:js', function() {
-  gulp.src([paths.js, '!' + paths.minJs], {
+  return gulp.src([paths.js, '!' + paths.minJs], {
       base: '.'
     })
     .pipe($.if(args.verbose, $.size({
@@ -64,7 +71,7 @@ gulp.task('min:js', function() {
  * --verbose: Displays the files matched by the globbing pattern.
  */
 gulp.task('min:css', function() {
-  gulp.src([paths.css, '!' + paths.minCss])
+  return gulp.src([paths.css, '!' + paths.minCss])
     .pipe($.if(args.verbose, $.size({
       showFiles: true,
       title: 'CSS'
@@ -75,6 +82,15 @@ gulp.task('min:css', function() {
 });
 
 gulp.task('min', ['min:js', 'min:css']);
+
+/**
+ * Watches the C# Razor views for changes, at which point the
+ * 'bootlint' task defined in the array is executed.
+ * Example: gulp watch
+ */
+gulp.task('watch', function () {
+    gulp.watch(paths.razorViews, ['bootlint']);
+});
 
 /**
  * Cleans the specified file path.
